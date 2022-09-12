a961041987e532a2d986fe5ca14612854975c904
Haofan Zheng <hzheng6@ucsc.edu>
2022-09-11 22:47:25 -0700

    made monitor report serializiable

--- include/EclipseMonitor/MonitorReport.hpp
+++ include/EclipseMonitor/MonitorReport.hpp
@@ -5,9 +5,7 @@
 
 #pragma once
 
-#include <cstdint>
-
-#include <array>
+#include "Internal/SimpleObj.hpp"
 
 /**
  * @brief Eclipse Monitor Secure Version Number (SVN) - Upper 1 byte
@@ -23,23 +21,96 @@
 namespace EclipseMonitor
 {
 
+namespace Internal
+{
+
+using MonitorIdTupleCore = std::tuple<
+	std::pair<
+		Obj::StrKey<SIMOBJ_KSTR("sessionID")>,
+		Obj::Bytes>
+>;
+
+using MonitorConfigTupleCore = std::tuple<
+	std::pair<
+		Obj::StrKey<SIMOBJ_KSTR("checkpointSize")>,
+		Obj::UInt64
+	>,
+	std::pair<
+		Obj::StrKey<SIMOBJ_KSTR("minDiffPercent")>,
+		Obj::UInt8
+	>,
+	std::pair<
+		Obj::StrKey<SIMOBJ_KSTR("maxWaitTime")>,
+		Obj::UInt64
+	>,
+	std::pair<
+		Obj::StrKey<SIMOBJ_KSTR("syncMaxWaitTime")>,
+		Obj::UInt64
+	>
+>;
+
+using MonitorSecStateTupleCore = std::tuple<
+	std::pair<
+		Obj::StrKey<SIMOBJ_KSTR("chainName")>,
+		Obj::String
+	>,
+	std::pair<
+		Obj::StrKey<SIMOBJ_KSTR("genesisHash")>,
+		Obj::Bytes
+	>,
+	std::pair<
+		Obj::StrKey<SIMOBJ_KSTR("checkpointIter")>,
+		Obj::UInt64
+	>,
+	std::pair<
+		Obj::StrKey<SIMOBJ_KSTR("checkpointHash")>,
+		Obj::Bytes
+	>
+>;
+
+} // namespace Internal
+
+
 /**
  * @brief Data used to identify the eclipse monitor instance, and it's
  *        not critical to security considerations
  *
  */
-struct MonitorId
+class MonitorId :
+	public Internal::Obj::StaticDict<Internal::MonitorIdTupleCore>
 {
-	using SessionIdType = std::array<uint8_t, 16>;
+public: // static members:
+
+	using Self = MonitorId;
+	using Base = Internal::Obj::StaticDict<Internal::MonitorIdTupleCore>;
+
+	template<typename _StrSeq>
+	using _StrKey = Internal::Obj::StrKey<_StrSeq>;
+	template<typename _StrSeq>
+	using _RetRefType = typename Base::template GetRef<_StrKey<_StrSeq> >;
+	template<typename _StrSeq>
+	using _RetKRefType = typename Base::template GetConstRef<_StrKey<_StrSeq> >;
+
+public:
+
+	using Base::Base;
 
 	/**
 	 * @brief The 128-bit session ID (randomly generated) used to identify
 	 *        a specific monitor process runtime
 	 *
 	 */
-	std::array<uint8_t, 16> m_sid;
+	_RetRefType<SIMOBJ_KSTR("sessionID")> get_sessionID()
+	{
+		return Base::template get<_StrKey<SIMOBJ_KSTR("sessionID")> >();
+	}
 
-}; // struct MonitorId
+	_RetKRefType<SIMOBJ_KSTR("sessionID")> get_sessionID() const
+	{
+		return Base::template get<_StrKey<SIMOBJ_KSTR("sessionID")> >();
+	}
+
+}; // class MonitorId
 
 
 /**
@@ -47,13 +118,38 @@ struct MonitorId
  *        security considerations
  *
  */
-struct MonitorConfig
+class MonitorConfig :
+	public Internal::Obj::StaticDict<Internal::MonitorConfigTupleCore>
 {
+public: // static members:
+
+	using Self = MonitorConfig;
+	using Base = Internal::Obj::StaticDict<Internal::MonitorConfigTupleCore>;
+
+	template<typename _StrSeq>
+	using _StrKey = Internal::Obj::StrKey<_StrSeq>;
+	template<typename _StrSeq>
+	using _RetRefType = typename Base::template GetRef<_StrKey<_StrSeq> >;
+	template<typename _StrSeq>
+	using _RetKRefType = typename Base::template GetConstRef<_StrKey<_StrSeq> >;
+
+public:
+
+	using Base::Base;
+
 	/**
 	 * @brief The number of blocks in each checkpoint window
 	 *
 	 */
-	uint64_t m_checkpointSize;
+	_RetRefType<SIMOBJ_KSTR("checkpointSize")> get_checkpointSize()
+	{
+		return Base::template get<_StrKey<SIMOBJ_KSTR("checkpointSize")> >();
+	}
+
+	_RetKRefType<SIMOBJ_KSTR("checkpointSize")> get_checkpointSize() const
+	{
+		return Base::template get<_StrKey<SIMOBJ_KSTR("checkpointSize")> >();
+	}
 
 	/**
 	 * @brief The "percentage" value used to calculate the minimum difficulty
@@ -63,7 +159,15 @@ struct MonitorConfig
 	 *        approximately "diff_min = diff_median * (m_minDiffPercent / 128)"
 	 *
 	 */
-	uint8_t  m_minDiffPercent;
+	_RetRefType<SIMOBJ_KSTR("minDiffPercent")> get_minDiffPercent()
+	{
+		return Base::template get<_StrKey<SIMOBJ_KSTR("minDiffPercent")> >();
+	}
+
+	_RetKRefType<SIMOBJ_KSTR("minDiffPercent")> get_minDiffPercent() const
+	{
+		return Base::template get<_StrKey<SIMOBJ_KSTR("minDiffPercent")> >();
+	}
 
 	/**
 	 * @brief The maximum time (in seconds) to wait for a new block. After this
@@ -72,7 +176,15 @@ struct MonitorConfig
 	 *        will be marked as inactive.
 	 *
 	 */
-	uint64_t m_maxWaitTime;
+	_RetRefType<SIMOBJ_KSTR("maxWaitTime")> get_maxWaitTime()
+	{
+		return Base::template get<_StrKey<SIMOBJ_KSTR("maxWaitTime")> >();
+	}
+
+	_RetKRefType<SIMOBJ_KSTR("maxWaitTime")> get_maxWaitTime() const
+	{
+		return Base::template get<_StrKey<SIMOBJ_KSTR("maxWaitTime")> >();
+	}
 
 	/**
 	 * @brief The maximum time (in seconds) to wait for the sync message to be
@@ -81,18 +193,108 @@ struct MonitorConfig
 	 *        needed.
 	 *
 	 */
-	uint64_t m_syncMaxWaitTime;
+	_RetRefType<SIMOBJ_KSTR("syncMaxWaitTime")> get_syncMaxWaitTime()
+	{
+		return Base::template get<_StrKey<SIMOBJ_KSTR("syncMaxWaitTime")> >();
+	}
 
-}; // struct MonitorConfig
+	_RetKRefType<SIMOBJ_KSTR("syncMaxWaitTime")> get_syncMaxWaitTime() const
+	{
+		return Base::template get<_StrKey<SIMOBJ_KSTR("syncMaxWaitTime")> >();
+	}
+
+}; // class MonitorConfig
+
+
+class MonitorSecurityState :
+	public Internal::Obj::StaticDict<Internal::MonitorSecStateTupleCore>
+{
+public: // static members:
+
+	using Self = MonitorSecurityState;
+	using Base = Internal::Obj::StaticDict<Internal::MonitorSecStateTupleCore>;
+
+	template<typename _StrSeq>
+	using _StrKey = Internal::Obj::StrKey<_StrSeq>;
+	template<typename _StrSeq>
+	using _RetRefType = typename Base::template GetRef<_StrKey<_StrSeq> >;
+	template<typename _StrSeq>
+	using _RetKRefType = typename Base::template GetConstRef<_StrKey<_StrSeq> >;
+
+public:
+
+	using Base::Base;
+
+	/**
+	 * @brief The name of the blockchain to be monitored (e.g., "bitcoin",
+	 *        "ethereum", and so on)
+	 *        NOTE: only ethereum is supported for now
+	 *
+	 */
+	_RetRefType<SIMOBJ_KSTR("chainName")> get_chainName()
+	{
+		return Base::template get<_StrKey<SIMOBJ_KSTR("chainName")> >();
+	}
+
+	_RetKRefType<SIMOBJ_KSTR("chainName")> get_chainName() const
+	{
+		return Base::template get<_StrKey<SIMOBJ_KSTR("chainName")> >();
+	}
+
+	/**
+	 * @brief The hash of the genesis block. This will be set by the monitor
+	 *        during the bootstrapI phase
+	 *
+	 */
+	_RetRefType<SIMOBJ_KSTR("genesisHash")> get_genesisHash()
+	{
+		return Base::template get<_StrKey<SIMOBJ_KSTR("genesisHash")> >();
+	}
+
+	_RetKRefType<SIMOBJ_KSTR("genesisHash")> get_genesisHash() const
+	{
+		return Base::template get<_StrKey<SIMOBJ_KSTR("genesisHash")> >();
+	}
+
+	/**
+	 * @brief The current iteration number/count of checkpoint windows.
+	 *        The number of the first checkpoint window containing the
+	 *        genesis block is 0
+	 *
+	 */
+	_RetRefType<SIMOBJ_KSTR("checkpointIter")> get_checkpointIter()
+	{
+		return Base::template get<_StrKey<SIMOBJ_KSTR("checkpointIter")> >();
+	}
+
+	_RetKRefType<SIMOBJ_KSTR("checkpointIter")> get_checkpointIter() const
+	{
+		return Base::template get<_StrKey<SIMOBJ_KSTR("checkpointIter")> >();
+	}
+
+	/**
+	 * @brief The hash of the last block in the current checkpoint window
+	 *
+	 */
+	_RetRefType<SIMOBJ_KSTR("checkpointHash")> get_checkpointHash()
+	{
+		return Base::template get<_StrKey<SIMOBJ_KSTR("checkpointHash")> >();
+	}
+
+	_RetKRefType<SIMOBJ_KSTR("checkpointHash")> get_checkpointHash() const
+	{
+		return Base::template get<_StrKey<SIMOBJ_KSTR("checkpointHash")> >();
+	}
+}; // class MonitorSecurityState
 
 
 inline MonitorConfig BuildDefaultMonitorConfig()
 {
 	MonitorConfig conf;
-	conf.m_checkpointSize  = 430;
-	conf.m_minDiffPercent  = 103; // which is around 80%
-	conf.m_maxWaitTime     = 400;
-	conf.m_syncMaxWaitTime = 13;
+	conf.get_checkpointSize()  = 430;
+	conf.get_minDiffPercent()  = 103; // which is around 80%
+	conf.get_maxWaitTime()     = 400;
+	conf.get_syncMaxWaitTime() = 13;
 
 	return conf;
 }
---
+++ test/src/TestMonitorReport.cpp
@@ -0,0 +1,84 @@
+// Copyright (c) 2022 Haofan Zheng
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+#include <array>
+
+#include <gtest/gtest.h>
+
+#include <EclipseMonitor/MonitorReport.hpp>
+#include <EclipseMonitor/EthHeaderMgr.hpp>
+
+#include "EthHeaders.hpp"
+
+namespace EclipseMonitor_Test
+{
+	extern size_t g_numOfTestFile;
+}
+
+using namespace EclipseMonitor;
+using namespace EclipseMonitor_Test;
+
+GTEST_TEST(TestMonitorReport, CountTestFile)
+{
+	static auto tmp = ++g_numOfTestFile;
+	(void)tmp;
+}
+
+GTEST_TEST(TestMonitorReport, MonitorId)
+{
+	{
+		auto testInput = SimpleObjects::Bytes({
+			0x00U, 0x01U, 0x02U, 0x03U, 0x04U, 0x05U, 0x06U, 0x07U,
+			0x00U, 0x01U, 0x02U, 0x03U, 0x04U, 0x05U, 0x06U, 0x07U,
+		});
+
+		MonitorId mId;
+		mId.get_sessionID() = testInput;
+		EXPECT_EQ(mId.get_sessionID(), testInput);
+
+		testInput[0] = 0x01U;
+		EXPECT_NE(mId.get_sessionID(), testInput);
+	}
+}
+
+GTEST_TEST(TestMonitorReport, MonitorConfig)
+{
+	{
+		MonitorConfig mConf = BuildDefaultMonitorConfig();
+		EXPECT_EQ(mConf.get_checkpointSize().GetVal(),  430);
+		EXPECT_EQ(mConf.get_minDiffPercent().GetVal(),  103);
+		EXPECT_EQ(mConf.get_maxWaitTime().GetVal(),     400);
+		EXPECT_EQ(mConf.get_syncMaxWaitTime().GetVal(), 13);
+	}
+}
+
+GTEST_TEST(TestMonitorReport, MonitorSecurityState)
+{
+	{
+		auto header00Bin = std::vector<uint8_t>(
+			GetEthHeaderBin_0().begin(), GetEthHeaderBin_0().end());
+		EthHeaderMgr header00 = EthHeaderMgr(header00Bin, 0);
+		auto header01Bin = std::vector<uint8_t>(
+			GetEthHeaderBin_1().begin(), GetEthHeaderBin_1().end());
+		EthHeaderMgr header01 = EthHeaderMgr(header01Bin, 0);
+
+		MonitorSecurityState mSecState;
+		mSecState.get_chainName()      = "ethereum";
+		mSecState.get_genesisHash()    =
+			header00.GetRawHeader().get_ParentHash();
+		mSecState.get_checkpointIter() = 12345;
+		mSecState.get_checkpointHash() =
+			header01.GetRawHeader().get_ParentHash();
+
+		EXPECT_EQ(mSecState.get_chainName(), "ethereum");
+		EXPECT_EQ(
+			mSecState.get_genesisHash(),
+			header00.GetRawHeader().get_ParentHash());
+		EXPECT_EQ(mSecState.get_checkpointIter().GetVal(), 12345);
+		EXPECT_EQ(
+			mSecState.get_checkpointHash(),
+			header01.GetRawHeader().get_ParentHash());
+	}
+}
