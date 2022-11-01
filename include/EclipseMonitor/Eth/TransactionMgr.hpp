eb599e180b9ebde58d53573c23e21448b9f34e49
kohdmonkey <atran18@ucsc.edu>
2022-11-01 11:29:06 -0700

    Parsing transaction rlp to retrieve contract address and input data

--- include/EclipseMonitor/Eth/TransactionMgr.hpp
+++ include/EclipseMonitor/Eth/TransactionMgr.hpp
@@ -6,11 +6,10 @@
 #pragma once
 
 #include <cstdint>
-
 #include <tuple>
 
-#include "../../Internal/SimpleObj.hpp"
-#include "../../Internal/SimpleRlp.hpp"
+#include "../Internal/SimpleObj.hpp"
+#include "../Internal/SimpleRlp.hpp"
 
 
 namespace EclipseMonitor
@@ -19,55 +18,118 @@ namespace Eth
 {
 
 
-enum class TnxVersion : uint8_t
+enum class TxnVersion : uint8_t
 {
 	Legacy  = 0,
-	Ver0x01 = 1,
-	Ver0x02 = 2,
-} // enum class TnxVersion
+	AccessList = 1,
+	DynamicFee = 2,
+}; // enum class TnxVersion
 
 
 class TransactionMgr
 {
 public: // static members:
 
-	static std::tuple<TnxVersion, Internal::Rlp::List> ParseTnx(
-		const Internal::Rlp::Bytes& rlpBytes
-	); // TODO[Tuan]
+	static std::tuple<TxnVersion, Internal::Obj::Object> ParseTnx(
+		const Internal::Obj::Bytes& rlpBytes
+	)
+	{
+		TxnVersion version;
+		uint8_t firstByte = rlpBytes[0];
+		Internal::Obj::Bytes innerRlp;
+		if (firstByte == 0x01)
+		{
+			version = TxnVersion::AccessList;
+			innerRlp = Internal::Obj::Bytes(rlpBytes.data() + 1, rlpBytes.data() + rlpBytes.size());
+		}
+		else if (firstByte == 0x02)
+		{
+			version = TxnVersion::DynamicFee;
+			innerRlp = Internal::Obj::Bytes(rlpBytes.data() + 1, rlpBytes.data() + rlpBytes.size());
+		}
+		else
+		{
+			version = TxnVersion::Legacy;
+			innerRlp = rlpBytes;
+		}
+
+		Internal::Obj::Object txObj
+			= Internal::Rlp::ParseRlp(innerRlp.GetVal());
+		return std::make_tuple(version, txObj);
+	}
 
 
 	static TransactionMgr FromBytes(
-		const Internal::Rlp::Bytes& rlpBytes
+		const Internal::Obj::Bytes& rlpBytes
 	)
 	{
-		TnxVersion ver;
-		Internal::Rlp::List body;
-		std::tie(ver, body) = ParseTnx(rlpBytes);
+		TxnVersion ver;
+		Internal::Obj::Object txnObj;
+		std::tie(ver, txnObj) = ParseTnx(rlpBytes);
 
-		return TransactionMgr(ver, body);
+		Internal::Obj::ListBaseObj &txnListObj = txnObj.AsList();
+
+		return TransactionMgr(ver, txnObj, txnListObj);
 	}
+
 
 public:
 
 	TransactionMgr(
-		TnxVersion version,
-		const Internal::Rlp::List& body
+		TxnVersion version,
+		const Internal::Obj::Object& txnObj,
+		Internal::Obj::ListBaseObj& txnListObj
 	) :
 		m_version(version),
-		m_tnxBody(body)
-	{};
+		m_txnObj(txnObj),
+		m_txnBody(txnListObj)
+	{
+		if (m_version == TxnVersion::Legacy)
+		{
+			Internal::Obj::BytesBaseObj& addr = m_txnBody[3].AsBytes();
+			m_contractAddr = Internal::Obj::Bytes(addr.data(), addr.data() + addr.size());
+
+			Internal::Obj::BytesBaseObj& data = m_txnBody[5].AsBytes();
+			m_data = Internal::Obj::Bytes(data.data(), data.data() + data.size());
+		}
+		else if (m_version == TxnVersion::AccessList)
+		{
+			Internal::Obj::BytesBaseObj& addr = m_txnBody[4].AsBytes();
+			m_contractAddr = Internal::Obj::Bytes(addr.data(), addr.data() + addr.size());
+
+			Internal::Obj::BytesBaseObj& data = m_txnBody[6].AsBytes();
+			m_data = Internal::Obj::Bytes(data.data(), data.data() + data.size());
+		}
+		else if (m_version == TxnVersion::DynamicFee)
+		{
+			Internal::Obj::BytesBaseObj& addr = m_txnBody[5].AsBytes();
+			m_contractAddr = Internal::Obj::Bytes(addr.data(), addr.data() + addr.size());
+
+			Internal::Obj::BytesBaseObj& data = m_txnBody[7].AsBytes();
+			m_data = Internal::Obj::Bytes(data.data(), data.data() + data.size());
+		}
+	};
 
 	// LCOV_EXCL_START
 	~TransactionMgr() = default;
 	// LCOV_EXCL_STOP
 
-	const Internal::Rlp::Bytes& GetContractAddr() const; // TODO[Tuan]
+	const Internal::Obj::Bytes& GetContractAddr() const
+	{
+		return m_contractAddr;
+	}
 
-	const Internal::Rlp::List& GetContactParams() const; // TODO[Tuan]
+	const Internal::Obj::Bytes& GetContactParams() const
+	{
+		return m_data;
+	}
 
 private:
-	TnxVersion m_version;
-	Internal::Rlp::List m_tnxBody;
+	TxnVersion m_version;
+	Internal::Obj::Bytes m_contractAddr;
+	Internal::Obj::Bytes m_data;
+	Internal::Obj::Object m_txnObj;
+	Internal::Obj::ListBaseObj& m_txnBody;
 };
 
 
